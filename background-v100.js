349bbb34bafce5dae0ef8e72282308900aceb4b7
niakw <foissy.tommy@gmail.com>
2026-08-18 02:14:24 +0200

    Wire persistent OUT metadata cache into 0.9.60

--- background-v100.js
+++ background-v100.js
@@ -35,6 +35,7 @@ const ISOLATED_RUNTIME=[
   'project-assignment-selfheal-v103.js',
   'breadcrumb-v100.js',
   'continuity-v100.js',
+  'out-state-cache-v110.js',
   'visual-stability-v101.js',
   'coach-v101.js',
   'polish-v090.js',
